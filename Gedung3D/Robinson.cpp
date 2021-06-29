eaebe914a782e68f4c9aa3fbbb4191346aec138b
rafael jonathan <rafaeljonathan117@gmail.com>
2021-06-29 18:44:54 +0700

    Update

--- Gedung3D/Robinson.cpp
+++ Gedung3D/Robinson.cpp
@@ -83,7 +83,8 @@ void display(void)
     glVertex3f(200.0, 10.0, 200.0);
     glVertex3f(200.0, 10.0, -200.0);
     glEnd();
-    
+
+    //di bawah ini adalah lobbyadsdadasdda
     
     glPopMatrix();
     glutSwapBuffers();
